f2d27b17be539c2cac5fceff8a6af8e968340d8d
NQTrung08 <nqtrung0810@gmail.com>
2024-10-08 15:18:36 +0700

    setup: add ssh key

--- packages/web/src/based/components/common/Slider.tsx
+++ packages/web/src/based/components/common/Slider.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Slider = () => {
+
+const Slider: React.FC = (
+  props
+) => {
   return (
-   
+
     <div className="h-full flex-1"> {/* Đảm bảo chiều cao đầy đủ */}
-      <img 
-        src="https://cdn1585.cdn4s4.io.vn//media/articles/865/content/cach-chon-plugin-slider-anh-cho-wordpress-phu-hop.jpg" 
-        alt="Slider" 
+      <img
+        src="https://cdn1585.cdn4s4.io.vn//media/articles/865/content/cach-chon-plugin-slider-anh-cho-wordpress-phu-hop.jpg"
+        alt="Slider"
         className="object-cover h-[400px] w-full rounded-md" // Kích thước ảnh
       />
     </div>
--- packages/web/src/based/components/common/Tbutton.tsx
+++ packages/web/src/based/components/common/Tbutton.tsx
@@ -8,8 +8,8 @@ interface Props {
 const Tbutton: React.FC<Props> = ({ title = '', onClick }) => {
   return (
     <div className="mt-12">
-      <button 
-        type="button" 
+      <button
+        type="button"
         className="w-full shadow-xl py-2.5 px-4 text-sm rounded-md text-white bg-green-500 hover:bg-green-200 focus:outline-none"
         onClick={onClick}
       >
